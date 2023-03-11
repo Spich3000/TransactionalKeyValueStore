85cb14b26df170af60cdd248b9d043ac49418123
Spich <spichakovda@gmail.com>
2023-03-12 02:03:22 +0300

    Initial Commit

---
+++ TransactionalKeyValueStore/TransactionalKeyValueStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionalKeyValueStoreApp.swift
+//  TransactionalKeyValueStore
+//
+//  Created by Дмитрий Спичаков on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TransactionalKeyValueStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
